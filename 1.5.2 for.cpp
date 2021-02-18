f047c63b8d604416a5ce513b6888f450b6fb4620
Alfredo-Palace <78890886+Alfredo-Palace@users.noreply.github.com>
2021-02-17 21:06:22 -0600

    Add files via upload

---
+++ 1.5.2 for.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/* cuenta los caracteres de la entrada; 2a. versión */
+int main()
+{
+long nc;
+for (nc = 0; getchar() != EOF; + + nc)
+printf("%.0f\n", nc);
+return 1;
+}
+
+
+
+
